85c70987327b88fba01ef79f3ad8162d69dc1758
sujeb2 <sdbb2q@gmail.com>
2024-08-27 18:49:30 +0900

    aw

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
 const toDiscord = document.querySelectorAll('.discordlogo');
+const copyServer = document.querySelectorAll('button-connect-server');
 
 // 각 텍스트를 클릭할 때마다 다른 alert 메시지를 표시
 toDiscord.forEach(toDiscord => {
