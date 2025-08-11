e83a0824f887986878a26b2dc2284076d62711c1
songro_ <sdbb2q@gmail.com>
2025-08-11 22:08:51 +0900

    Delete src/script.js

--- src/script.js
+++
@@ -1,9 +0,0 @@
-const toDiscord = document.querySelectorAll('.discordlogo');
-const copyServer = document.querySelectorAll('button-connect-server');
-
-// 각 텍스트를 클릭할 때마다 다른 alert 메시지를 표시
-toDiscord.forEach(toDiscord => {
-    toDiscord.addEventListener('click', () => {
-        window.location.href = "https://discord.gg/sJ8eKW8r6s";
-    });
-});
